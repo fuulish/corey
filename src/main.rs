98feac720136b24a0d3865df2c918ecf01570f43
Frank Uhlig <uhlig.frank@gmail.com>
2023-10-26 22:02:35 +0200

    chg: switch to using authentication token

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use reqwest::Error;
 use serde::Deserialize;
 
+use std::env;
+
 #[derive(Deserialize, Debug)]
 struct User {
     login: String,
@@ -16,10 +18,28 @@ fn main() -> Result<(), Error> {
         repo = "rust-cookbook"
     );
     println!("{}", request_url);
-    let client = reqwest::blocking::Client::builder()
-        .user_agent("clireview/0.0.1")
+    let request = reqwest::blocking::Client::new()
+        .post("https://api.github.com/graphql")
+        .bearer_auth("MY_GH_TOKEN")
         .build()?;
-    let response = client.get(&request_url).send()?;
+
+    let token = env::var("TOKEN").unwrap();
+
+    let client = reqwest::blocking::Client::new()
+        .get(request_url)
+        .header("User-Agent", "clireview/0.0.1")
+        .bearer_auth(token);
+    let response = client.send()?;
+    /*
+    let response = Client::new()
+        .get(build_github_access_data_url())
+        .header("Accept", "application/json")
+        .header("User-Agent", "Rust")
+        .bearer_auth(token)
+        .send()
+        .await?;
+    */
+    // .user_agent("clireview/0.0.1")
 
     let users: Vec<User> = response.json()?;
     println!("{:?}", users);
