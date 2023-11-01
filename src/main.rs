d2802d15f0bc1728e5029ed424202a8164619542
Frank Uhlig <uhlig.frank@gmail.com>
2023-11-01 21:27:54 +0100

    chg(structure): refactoring

--- src/main.rs
+++ src/main.rs
@@ -67,56 +67,124 @@ enum ReviewInterface {
 // XXX: a review should neglect the infrastructure things (to a large degree)
 //      keep a list of comments and the views into it being original comments and replies
 //      -> one private member and two public view
+//      on the contrary, we'll keep only the review infrastructure things here
+//      ...and treat the comments and a view into it separately
 // XXX: move infrastructure related things to ReviewInterface (and have that use the platform enum)
 #[derive(Debug, Serialize, Deserialize)]
 struct Review {
     interface: ReviewInterface,
     owner: String,
     repo: String,
+    auth: String,
     url: String,
     id: u32,
-    pub comments: Vec<ReviewComment>,
-    // XXX: maybe add review comments here as well
-    //      they do belong to the review, after all
+}
+
+// XXX: having reviewcomments in here would have been nice
+//      however, it generates a self-referential struct, which does not work out of the box in rust
+struct Conversation<'a> {
+    pub starter: HashMap<u32, &'a ReviewComment>,
+    pub replies: HashMap<u32, Vec<&'a ReviewComment>>,
+}
+
+impl<'a> Conversation<'a> {
+    pub fn from_review_comments(comments: &'a Vec<ReviewComment>) -> Result<Self, Error> {
+        let starter: HashMap<_, _> = comments
+            .iter()
+            .filter(|x| None == x.in_reply_to_id)
+            .map(|c| (c.id, c))
+            .collect();
+
+        let mut replies: HashMap<u32, Vec<&ReviewComment>> = HashMap::new();
+
+        // XXX: depending on size of review, this will not be cheap
+        //      however, for the typical size of reviews, we are talking
+        //      about, this will not be expensive either
+        //          XXX: optimize, when the need arises
+        for (k, _) in starter.iter() {
+            replies.insert(
+                *k,
+                comments
+                    .iter()
+                    .filter(|x| {
+                        if let Some(id) = x.in_reply_to_id {
+                            if id == *k {
+                                true
+                            } else {
+                                false
+                            }
+                        } else {
+                            false
+                        }
+                    })
+                    .collect(),
+            );
+        }
+        let replies = replies; // don't need this to be mutable any longer
+
+        Ok(Conversation { starter, replies })
+    }
+    pub fn print(&self) {
+        // pretty printing of conversations
+        for (id, comment) in self.starter.iter() {
+            // XXX: I always forget, do we need this explicit
+            //      iter call?
+            println!("|{}|", "+".repeat(NCOL));
+            println!("{}", comment.path);
+            println!("{}", comment.diff_hunk);
+            println!(
+                "{name}: {body}",
+                name = comment.user.login,
+                body = comment.body
+            );
+            match self.replies.get(id) {
+                None => {
+                    println!("|{}|", "-".repeat(NCOL));
+                    continue;
+                }
+                Some(rid) => {
+                    // XXX: sort replies by ids (if required)
+                    for reply in rid {
+                        // XXX error handling
+                        println!("{name}: {body}", name = reply.user.login, body = reply.body);
+                    }
+                    println!("|{}|", "-".repeat(NCOL));
+                }
+            }
+        }
+    }
 }
 
 impl Review {
-    pub fn from_args(args: &Args, token: &str) -> Result<Self, Error> {
+    pub fn from_args(args: &Args) -> Result<Self, Error> {
         Ok(Review {
             interface: args.platform,
             owner: args.owner.to_owned(),
             repo: args.repo.to_owned(),
             url: args.url.to_owned(),
             id: args.id,
-            comments: Review::get_review_comments(
-                &args.owner,
-                &args.repo,
-                &args.url,
-                args.id,
-                token,
-                args.platform,
-            )?,
+            auth: args.token.to_owned(),
         })
     }
-    fn get_review_comments(
-        owner: &str,
-        repo: &str,
-        url: &str,
-        prnum: u32,
-        token: &str,
-        interface: ReviewInterface,
-    ) -> Result<Vec<ReviewComment>, Error> {
-        let request_url = match interface {
+
+    fn get_authentication(&self) -> Result<String, Error> {
+        fs::read_to_string(&self.auth).map_err(Error::from_io_error)
+    }
+    pub fn get_comments(&self) -> Result<Vec<ReviewComment>, Error> {
+        let request_url = match self.interface {
             ReviewInterface::GitHub => {
                 format!(
                     "https://api.{url}/repos/{owner}/{repo}/pulls/{prnum}/comments",
-                    owner = owner,
-                    repo = repo,
-                    url = url,
-                    prnum = prnum,
+                    owner = self.owner,
+                    repo = self.repo,
+                    url = self.url,
+                    prnum = self.id,
                 )
             }
         };
+
+        let token = self.get_authentication()?;
+
         let client = reqwest::blocking::Client::new()
             .get(request_url)
             .header("User-Agent", "clireview/0.0.1")
@@ -125,6 +193,15 @@ impl Review {
         let response = client.send().map_err(Error::from_reqwest_error)?;
 
         response.json().map_err(Error::from_reqwest_error)
+    }
+    pub fn save_config(&self) -> Result<(), Error> {
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("review.yml")
+            .expect("Couldn't open file");
+        serde_yaml::to_writer(f, &self).unwrap(); // XXX: return proper error
+        Ok(())
     }
 }
 
@@ -172,84 +249,16 @@ struct Args {
 fn main() -> Result<(), Error> {
     let args = Args::parse();
 
-    let token = fs::read_to_string(&args.token).map_err(Error::from_io_error)?;
-
     let pr = match args.command {
-        Command::Init => Review::from_args(&args, &token)?,
+        Command::Init => Review::from_args(&args)?,
         Command::Update => return Err(Error::NotImplemented), // read config and update comments
     };
 
-    let f = std::fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open("review.yml")
-        .expect("Couldn't open file");
-    serde_yaml::to_writer(f, &pr).unwrap();
+    pr.save_config().unwrap(); // XXX: return proper error
 
-    // XXX: sort into two hashmaps
-    //      - one with original ids
-    //      - one with replies to original ids
+    let comments = pr.get_comments()?;
+    let conversation = Conversation::from_review_comments(&comments)?;
 
-    let original_ids: HashMap<_, _> = pr
-        .comments
-        .iter()
-        .filter(|x| None == x.in_reply_to_id)
-        .map(|c| (c.id, c))
-        .collect();
-
-    let mut reply_ids: HashMap<u32, Vec<&ReviewComment>> = HashMap::new();
-
-    // XXX: depending on size of review, this will not be cheap
-    //      however, for the typical size of reviews, we are talking
-    //      about, this will not be expensive either
-    //          XXX: optimize, when the need arises
-    for (k, _) in original_ids.iter() {
-        reply_ids.insert(
-            *k,
-            pr.comments
-                .iter()
-                .filter(|x| {
-                    if let Some(id) = x.in_reply_to_id {
-                        if id == *k {
-                            true
-                        } else {
-                            false
-                        }
-                    } else {
-                        false
-                    }
-                })
-                .collect(),
-        );
-    }
-    let reply_ids = reply_ids; // don't need this to be mutable any longer
-
-    // pretty printing of conversations
-    for (id, comment) in original_ids.iter() {
-        // XXX: I always forget, do we need this explicit
-        //      iter call?
-        println!("|{}|", "+".repeat(NCOL));
-        println!("{}", comment.path);
-        println!("{}", comment.diff_hunk);
-        println!(
-            "{name}: {body}",
-            name = comment.user.login,
-            body = comment.body
-        );
-        match reply_ids.get(id) {
-            None => {
-                println!("|{}|", "-".repeat(NCOL));
-                continue;
-            }
-            Some(rid) => {
-                // XXX: sort replies by ids (if required)
-                for reply in rid {
-                    // XXX error handling
-                    println!("{name}: {body}", name = reply.user.login, body = reply.body);
-                }
-                println!("|{}|", "-".repeat(NCOL));
-            }
-        }
-    }
+    conversation.print();
     Ok(())
 }
